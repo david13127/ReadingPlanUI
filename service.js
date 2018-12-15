824109d7a6cf9e03c3f15015c775c9e8e6e58516
david13127 <zhanglong9313@qq.com>
2018-12-15 12:33:42 +0800

    修改工程名

--- service.js
+++ service.js
@@ -1,5 +1,5 @@
 var connect = require('connect');
     serveStatic = require('serve-static');
 var app = connect();
-app.use(serveStatic("../demo"));
+app.use(serveStatic("../ReadingPlanUI"));
 app.listen(5000);
